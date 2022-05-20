ff2a493a1036ace49fbff06dea7ec6150beb7129
Matt Kane <m@mk.gg>
2022-05-20 11:02:59 +0100

    chore: add test site

---
+++ e2e-tests/default/gatsby-config.js
@@ -0,0 +1,20 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Image Runner`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ e2e-tests/default/src/pages/index.tsx
@@ -0,0 +1,99 @@
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+const headingAccentStyles = {
+  color: "#663399",
+};
+const paragraphStyles = {
+  marginBottom: 48,
+};
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+};
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+};
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
+};
+
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+};
+
+const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: "none",
+  marginBottom: 24,
+};
+
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
+};
+
+const docLink = {
+  text: "TypeScript Documentation",
+  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
+  color: "#8954A8",
+};
+
+const badgeStyle = {
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
+  fontSize: 11,
+  fontWeight: "bold",
+  letterSpacing: 1,
+  borderRadius: 4,
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative" as "relative",
+  top: -2,
+  marginLeft: 10,
+  lineHeight: 1,
+};
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <h1 style={headingStyles}>
+        Congratulations
+        <br />
+        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        🎉🎉🎉
+      </h1>
+
+      <StaticImage src="../images/icon.png" alt="Gatsby" />
+    </main>
+  );
+};
+
+export default IndexPage;
