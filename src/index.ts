d61d8c0f08c965351f9ddf96d0136719cce508f4
Matt Kane <m@mk.gg>
2022-05-20 09:27:34 +0100

    chore: move back to separate export
    This reverts commit b63120f7cb6e9a841b6b1f6fe34dab9577986fe7.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
-export { onBuild } from './plugin'
-export { handler } from './handler'
+import type { OnBuild } from '@netlify/build'
+import { existsSync } from 'fs'
+import { ensureDir, writeFile } from 'fs-extra'
+import { posix as path } from 'path'
+import { greenBright } from 'chalk'
+
+export const onBuild: OnBuild = async ({ constants, netlifyConfig }) => {
+  const cacheDir = path.resolve(
+    constants.PUBLISH_DIR,
+    '..',
+    '.cache',
+    'caches',
+    'gatsby-runner'
+  )
+  if (!existsSync(path.join(cacheDir, '.did-run'))) {
+    console.log(
+      `The build runner did not run. Please change your build command to ${greenBright`gatsby-runner`} and try again`
+    )
+    return
+  }
+  await ensureDir(constants.INTERNAL_FUNCTIONS_SRC)
+  await writeFile(
+    path.join(constants.INTERNAL_FUNCTIONS_SRC, 'gatsby-image.ts'),
+    `export { handler } from '@netlify/gatsby-runner'`
+  )
+  netlifyConfig.functions['gatsby-image'] = {
+    external_node_modules: [
+      'keyv',
+      'mozjpeg',
+      'pngquant-bin',
+      'gatsby-core-utils',
+    ],
+    included_files: [path.join(cacheDir, '**', '*.json')],
+  }
+  netlifyConfig.redirects.push({
+    from: '/static/:sourceDigest/:queryDigest/:filename',
+    to: '/.netlify/builders/gatsby-image',
+    status: 200,
+  })
+}
