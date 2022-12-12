e4d6063232fdc7c13f72c21f863c4e789edb1f05
Erica Pisani <pisani.erica@gmail.com>
2022-12-12 15:09:24 -0500

    test: add projectId
    reduce amount of containers

--- cypress/config/ci.config.ts
+++ cypress/config/ci.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     // The baseUrl is set within `.github/workflows/cypress-demo.yml`
-    projectId: "vq2rbp"
+    projectId: "vq2rbp",
   },
 });
